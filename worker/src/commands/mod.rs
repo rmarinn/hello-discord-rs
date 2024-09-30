a4256b48e76bc103e0ded373255e7d22a98a52d0
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-10-01 03:39:47 +0800

    feat: create test for `roll()`

--- worker/src/commands/mod.rs
+++ worker/src/commands/mod.rs
@@ -2,11 +2,12 @@ mod roll;
 
 use roll::*;
 use serde_discord::interaction::CommandInteractionData;
-use worker::Response;
+use worker::{console_debug, Response};
 
 pub fn handle_cmd(cmd: CommandInteractionData) -> Result<Response, worker::Error> {
+    console_debug!("GOT: {:?}", cmd);
     match cmd.name() {
-        "roll" => roll(cmd),
+        "roll" => Response::from_json(&roll(cmd)),
         _ => Response::error("Command not implemented", 501),
     }
 }
--- worker/src/commands/roll.rs
+++ worker/src/commands/roll.rs
@@ -4,16 +4,17 @@ use serde_discord::{
     response::{data::MessageBuilder, InteractionResponse},
     types::MultiTypeValue,
 };
-use worker::Response;
 
-pub fn roll(cmd: CommandInteractionData) -> Result<Response, worker::Error> {
+pub fn roll(cmd: CommandInteractionData) -> InteractionResponse {
     let mut min_val: i64 = 1;
     let mut max_val: i64 = 6;
 
     if let Some(opt) = cmd.option("min") {
         if let Some(val) = opt.value() {
             match val {
-                MultiTypeValue::Integer(val) => min_val = *val,
+                MultiTypeValue::Integer(val) => {
+                    min_val = *val;
+                }
                 _ => (),
             }
         }
@@ -22,7 +23,9 @@ pub fn roll(cmd: CommandInteractionData) -> Result<Response, worker::Error> {
     if let Some(opt) = cmd.option("max") {
         if let Some(val) = opt.value() {
             match val {
-                MultiTypeValue::Integer(val) => max_val = *val,
+                MultiTypeValue::Integer(val) => {
+                    max_val = *val;
+                }
                 _ => (),
             }
         }
@@ -35,6 +38,74 @@ pub fn roll(cmd: CommandInteractionData) -> Result<Response, worker::Error> {
         .content(rand_number.to_string())
         .build();
 
-    let resp = InteractionResponse::Message(msg);
-    Response::from_json(&resp)
+    InteractionResponse::Message(msg)
+}
+
+#[cfg(test)]
+mod test {
+    use core::panic;
+
+    use serde_discord::{
+        interaction::CommandInteractionData,
+        response::InteractionResponse,
+        types::{CommandOptionKind, MultiTypeValue},
+    };
+
+    use super::roll;
+
+    #[test]
+    fn can_roll_within_range() {
+        // check if setting min/max works
+        for i in 1..100 {
+            let opt_min = CommandInteractionData::new(
+                "min",
+                CommandOptionKind::Integer,
+                Some(MultiTypeValue::Integer(i)),
+                None,
+                None,
+            );
+            let opt_max = CommandInteractionData::new(
+                "max",
+                CommandOptionKind::Integer,
+                Some(MultiTypeValue::Integer(i)),
+                None,
+                None,
+            );
+            let cmd = CommandInteractionData::new(
+                "roll",
+                CommandOptionKind::SubCommand,
+                None,
+                Some(vec![opt_min, opt_max]),
+                None,
+            );
+
+            match roll(cmd) {
+                InteractionResponse::Message(msg) => {
+                    let num: i64 = msg.content().clone().unwrap().parse().unwrap();
+                    assert_eq!(num, i);
+                }
+                _ => panic!("should be InteractionResposne::Message"),
+            }
+        }
+
+        // check if default rolls from 1-6
+        for _ in [1..100] {
+            let cmd = CommandInteractionData::new(
+                "roll",
+                CommandOptionKind::SubCommand,
+                None,
+                None,
+                None,
+            );
+
+            match roll(cmd) {
+                InteractionResponse::Message(msg) => {
+                    let num: u64 = msg.content().clone().unwrap().parse().unwrap();
+                    assert!(num >= 1);
+                    assert!(num <= 6);
+                }
+                _ => panic!("should be InteractionResposne::Message"),
+            }
+        }
+    }
 }
